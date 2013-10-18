537f4e732c058a13c079f252575f4996e34e876e
varmara <marina.nikolaeva@gmail.com>
2013-10-18 12:41:02 +0400

    added function extract coefficients of model II regressions and confidence intervals from lmodel2 object
    returns dataframe containing intercepts and slopes of model II
    regression lines. Using this dataframe one can plot model II regression
    line and confidence intervals in ggpolt.

---
+++ w3-regression2/int_slope_lmodel2.R
@@ -0,0 +1,37 @@
+int_slope_lmodel2 <- function(fit, method = "RMA")
+  # computes intercepts and slopes of fitted and confidence limits' bands for model II regression fitted by lmodel2
+  # fit: a lmodel2 object
+  # method: OLS, MA, SMA, RMA
+  # returns a data.frame
+  # based on code lmodel2 code by Pierre Legendre
+  # modified by Marina Varfolomeeva
+{
+  centr.y <- mean(fit$y)
+  centr.x <- mean(fit$x)
+  row <- which(fit$regression.results == method)
+  a <- fit$regression.results[row,2]
+  b <- fit$regression.results[row,3]
+  b1 <- fit$confidence.intervals[row,4]
+  a1 <- centr.y - b1*centr.x
+  b2 <- fit$confidence.intervals[row,5]
+  a2 <- centr.y - b2*centr.x
+  res <- data.frame(line = c("predicted", "lower", "upper"), 
+                    intercept = c(a, a1, a2),
+                    slope = c(b, b1, b2))
+  if((row != 1) && (fit$rsquare <= fit$epsilon)) {
+    warning("R-square = 0: model and C.I. not drawn for MA, SMA or RMA")
+  }
+  return(res)
+}
+
+# # Example usage of int_slope_lmodel2()
+# library(ggplot2)
+# library(lmodel2)
+# data(cars)
+# fit <- lmodel2(dist ~ speed, range.y="interval", range.x="interval", data=cars, nperm = 100)
+# reg_lines <- int_slope_lmodel2(fit)
+# ggplot(cars) + 
+#   geom_point(data = cars, aes(x = speed, y = dist)) +
+#   geom_abline(data = reg_lines, aes(intercept = intercept, slope = slope), colour = c("blue", "red", "red"))
+# # Compare with built in plot
+# plot(fit, "RMA")
